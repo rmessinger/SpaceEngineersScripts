3a173f299d0b5017c7e2dea35f99b2e42d3680b4
Reid Messinger <reid.messinger@gmail.com>
2020-03-03 21:17:56 -0500

    Set thruster percentage

--- SpaceEngineers/AutoPilot.cs
+++ SpaceEngineers/AutoPilot.cs
@@ -15,23 +15,26 @@ namespace SpaceEngineers.Flight
     {
         List<IMyThrust> controlledThrusters = new List<IMyThrust>();
         List<IMyShipConnector> shipConnectors = new List<IMyShipConnector>();
-        IMyShipConnector connector;
-        VRage.Game.ModAPI.Ingame.IMyCubeGrid grid;
-        long lastTime;
+        VRage.Game.ModAPI.Ingame.IMyCubeGrid shipGrid;
+        System.DateTime lastTime;
         Vector3D lastPosition;
+        double maxSpeed;
+        float cruiseSpeed;
+        float minSpeed;
 
         public AutoPilot()
         {
             Runtime.UpdateFrequency = UpdateFrequency.Update100;
-            grid = Me.CubeGrid;
+            shipGrid = Me.CubeGrid;
+            maxSpeed = 0;
+            cruiseSpeed = 105;
+            minSpeed = 40;
 
             List<IMyShipConnector> allConnectors = new List<IMyShipConnector>();
             GridTerminalSystem.GetBlocksOfType<IMyShipConnector>(allConnectors);
-            Echo("Me.CubeGrid=" + grid.Name);
             foreach(IMyShipConnector connector in allConnectors)
             {
-                Echo("connector.CubeGrid=" + connector.CubeGrid);
-                if (connector.CubeGrid.IsSameConstructAs(grid))
+                if (connector.CubeGrid.IsSameConstructAs(shipGrid))
                 {
                     shipConnectors.Add(connector);
                 }
@@ -40,19 +43,37 @@ namespace SpaceEngineers.Flight
 
         public void Main()
         {
-            if (ConnectorsLocked(shipConnectors)
+            if (ConnectorsLocked(shipConnectors))
             {
-                // return;
+                return;
             }
             else if (controlledThrusters.Count == 0)
             {
-                // InitializeThrusters();
+                InitializeThrusters();
             }
 
-            lastPosition = grid.GetPosition();
-            StringBuilder pos = new StringBuilder();
-            pos.Append($"X: {lastPosition.X}\nY: {lastPosition.Y}\nZ: {lastPosition.Z}");
-            WriteToLCD(pos.ToString());
+            DateTime now = DateTime.Now;
+            Vector3D position = shipGrid.GetPosition();
+            double velocity = CalculateVelocity(lastPosition, position, lastTime, now);
+
+            lastTime = now;
+            lastPosition = shipGrid.GetPosition();
+
+            if (velocity > maxSpeed)
+            {
+                maxSpeed = velocity;
+            }
+
+            float target = 0;
+            if (velocity > minSpeed)
+            {
+                target = SetThrusters(velocity);
+            }
+
+            StringBuilder displayText = new StringBuilder();
+            displayText.Append($"Set speed:\n{Math.Round(target, 3)}");
+            // displayText.Append($"X: {lastPosition.X}\nY: {lastPosition.Y}\nZ: {lastPosition.Z}");
+            WriteToLCD(displayText.ToString());
         }
 
         public void InitializeThrusters()
@@ -64,12 +85,24 @@ namespace SpaceEngineers.Flight
             {
                 // Echo(thruster.GridThrustDirection.Equals(VRageMath.Vector3I.Forward).ToString());
                 if (thruster.GridThrustDirection.Equals(VRageMath.Vector3I.Backward)
-                    && thruster.CustomName.Contains("Large Atmospheric"))
+                    && thruster.CubeGrid.IsSameConstructAs(shipGrid))
                 {
                     controlledThrusters.Add(thruster);
                     Echo(thruster.CustomName);
                 }
             }
+        }
+
+        public float SetThrusters(double velocity)
+        {
+            float thrustPercentage = 1 - (Math.Max(0, 85 - (float)velocity) / cruiseSpeed);
+
+            foreach(IMyThrust thruster in controlledThrusters)
+            {
+                thruster.ThrustOverridePercentage = thrustPercentage;
+            }
+
+            return thrustPercentage;
         }
 
         public void WriteToLCD(string message)
@@ -95,6 +128,17 @@ namespace SpaceEngineers.Flight
             }
 
             return true;
+        }
+
+        private double CalculateVelocity(Vector3D startPosition, Vector3D endPosition, DateTime startTime, DateTime endTime)
+        {
+            double elapsed = (endTime - startTime).TotalSeconds;
+            double xDistance = endPosition.X - startPosition.X;
+            double yDistance = endPosition.Y - startPosition.Y;
+            double zDistance = endPosition.Z - startPosition.Z;
+            double distance = Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2) + Math.Pow(zDistance, 2));
+
+            return distance / elapsed;
         }
     }
 }
