5e9ca7b902a2a87e7849a5f07603ecd951e26d63
Reid Messinger <reid.messinger@gmail.com>
2020-03-25 20:59:45 -0400

    Add comments

--- Exporter/Exporter.py
+++ Exporter/Exporter.py
@@ -20,7 +20,6 @@ with open(file_path, 'r') as file:
     file_data = file.read().split('\n')
 
 lines_to_skip = 0
-trailing_braces = 0
 de_indent = 0
 expected_endbraces = 0
 
@@ -28,32 +27,48 @@ for line in file_data:
     if lines_to_skip > 0:
         lines_to_skip -= 1
         continue
+    # using lines can be safely skipped without any later considerations
     if 'using' in line:
         continue
-    if ': MyGridProgram' in line:
-        trailing_braces += 1
-        lines_to_skip = 1
-        de_indent += spaces_per_tab
-        continue
+    # namespace enclosures should be skipped
     if 'namespace' in line:
-        trailing_braces += 1
+        # so should their subsequent opening brace
         lines_to_skip = 1
+        # removing this level of scope requires de-indenting
         de_indent += spaces_per_tab
         continue
+    # extension of MyGridProgram signifies this begins the class definition
+    if ': MyGridProgram' in line:
+        # class definitions should also trim the following opening brace
+        lines_to_skip = 1
+        # add this to the indent butcher's bill
+        de_indent += spaces_per_tab
+        continue
+    # SE programmable blocks are all of class "Program"
+    # replace the class-specific constructor with one the game compiler won't hate
     if 'public ' + file_name in line:
         line = line.replace(file_name, 'Program')
+    # If we've gotten this far, this brace begins a valid scope within the in-game class
+    # increment the number of expected closing braces so that they don't get skipped
     if '{' in line:
         expected_endbraces += 1
+    # Closing brace. Check if we've been expecting one of those
+    # this should only be false for the last few lines of the file
     if '}' in line:
         if expected_endbraces > 0:
             expected_endbraces -= 1
         else:
             continue
+    # Indents that don't match the surrounding style are gross
+    # De-indent all lines by the amount subtracted by removing class, namespace, etc
+    # But only remove whitespace
     indent = len(line) - len(line.lstrip(' '))
     if indent >= de_indent:
         line = line[de_indent:]
     output_buffer.append(line)
 
+# Join all these line entries into a single string
 separator = '\n'
 output = separator.join(output_buffer)
+# Shoot it into the clipboard
 pyperclip.copy(output)
